da21a57b226eb420999213c5cbe6302a26f1fec5
DidierSpaier <didier@slint.fr>
2025-05-04 11:42:20 +0200

    clean-kernels.sh updated.

--- clean-kernels.sh
+++ clean-kernels.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 # shellcheck disable=SC1091,SC2034
-# We remove all packages kernel, kernel-source and kernel headers that are
-# with a version number not matching that of the running kernel
+# This script has been written from scratch and dedicated to the public
+# domain by its author: Didier Spaier, Paris.
+# For more information, please refer to <http://unlicense.org/>
+
+# Propose to remove all packages kernel, kernel-source and kernel-headers
+# with a version number not matching that of the running kernel.
+# Remove the iniramfs whose version number that of a running kernel.
 TEXTDOMAIN=slint-scripts
 if [ ! "$(id -u)" -eq 0 ]; then
 	gettext "Please run this script as root."; echo
@@ -50,13 +55,13 @@ if [ -s "$_installed" ]; then
 		unset ANSWER
 		VERSION=$(echo "$i"|sed "s/.*-\($FRAG\)-$FRAG-$FRAG$/\1/")
 		if grep -q "$VERSION" "$_versions"; then
-			echo "not removing $i as its version matches that of an installed kernel package." >>$LOG
+			echo "Not removing $i as its version matches that of an installed kernel package." >>$LOG
 			continue
 		fi
 		while [ ! "$(to_lower "$ANSWER")" = "yes" ] && [ ! "$(to_lower "$ANSWER")" = "no" ]; do
-			eval_gettext "The version of \$i does not match that of kernel package. Do you want to remove it?"
+			eval_gettext "The version of \$i does not match that of an installed kernel package."
 			echo
-			gettext "Type yes or no: "
+			gettext "Do you want to remove it? Type yes or no: "
 			read -r ANSWER
 			[ ! "$(to_lower "$ANSWER")" = "yes" ] && continue
 			echo "spkg -d $i" >>$LOG
@@ -71,13 +76,13 @@ if [ -s "$_installed" ]; then
 		unset ANSWER
 		VERSION=$(echo "$i"|sed "s/.*-\($FRAG\)-$FRAG-$FRAG$/\1/")
 		if grep -q "$VERSION" "$_versions"; then
-			echo "not removing $i as its version matches that of an installed kernel package." >>$LOG
+			echo "Not removing $i as its version matches that of an installed kernel package." >>$LOG
 			continue
 		fi
 		while [ ! "$(to_lower "$ANSWER")" = "yes" ] && [ ! "$(to_lower "$ANSWER")" = "no" ]; do
-			eval_gettext "The version of \$i does not match that of kernel package. Do you want to remove it?"
+			eval_gettext "The version of \$i does not match that of an installed kernel package."
 			echo
-			gettext "Type yes or no: "
+			gettext "Do you want to remove it? Type yes or no: "
 			read -r ANSWER
 			[ ! "$(to_lower "$ANSWER")" = "yes" ] && continue
 			echo "spkg -d $i" >>$LOG
@@ -92,7 +97,7 @@ find . -name "initramfs*img"|sed "s#..##"|while read -r i; do
 	iVERSION="$(printf "%b" "${i%.img}"|sed "s#initramfs-##")"
 	if ! find . -name "vmlinuz*"|grep -q "$iVERSION"; then
 		rm "$i"
-		eval_gettext "stale initramfs \$i removed."
+		eval_gettext "Stale initramfs \$i removed."
 		echo
 	fi
 done
